a1c0dd5dba6c6ef67d9ae037061a1a522bab2ecf
Vox <116598880+Vox314@users.noreply.github.com>
2024-01-01 18:27:27 +0100

    messed up a comment bru

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ def on_focus_out(event):
         file_path_entry.insert(0, "path/to/file")
         file_path_entry.configure(text_color="grey")
 
-# Rootr Frame
+# Root Frame
 root = ctk.CTk()
 if platform.system() == 'Windows':
     root.iconbitmap(os.path.join('resources', 'icon.ico'))
